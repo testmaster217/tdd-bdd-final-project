250240d13b7a7e2bc750643afffc9633ea90d843
Collin Vesel <cvesel217@gmail.com>
2025-03-22 00:14:35 -0400

    Added a step implementation for finding specific text in the search results.

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -135,3 +135,13 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results),
+            name
+        )
+    )
+    assert(found)
