100393504833af6f3e3c2d911da2833c597ffa11
Collin Vesel <cvesel217@gmail.com>
2025-03-22 00:17:27 -0400

    Added a step implementation for specific text not being in the search results.

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -140,8 +140,14 @@ def step_impl(context, element_name, text_string):
 def step_impl(context, name):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
         expected_conditions.text_to_be_present_in_element(
-            (By.ID, 'search_results),
+            (By.ID, 'search_results'),
             name
         )
     )
     assert(found)
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    search_results = context.driver.find_element(By.ID, 'search_results')
+    assert(name not in search_results.text)
+    
